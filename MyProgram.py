4b12a876079851cdf490370c33e468f70b1a7e77
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-06-30 18:50:49 +0530

    Add files via upload

---
+++ MyProgram.py
@@ -0,0 +1,69 @@
+str1 = "jdashaahakfmwoeyoy"
+print(str1.upper())
+
+str2 = "JFAFAFHHKHFHFFJSALF"
+print(str2.lower())
+
+str3 = " Akash Thorat"
+print(str3.strip)
+
+str4 = "Akash Thorat !!!"
+print(str4.rstrip("!"))
+
+str5 = "i am the akash"
+print(str5.replace("akash", "naruto"))
+
+str6 = "Akash Thorat"
+print(str6.split(" "))
+
+str7 = "naruto"
+capstr7 = str7.capitalize()
+print(capstr7)
+
+str8 = "naruto tak de bayo"
+print(str8.center(40))
+
+str9 = "lkghgahghsaljhglksglskghsghggjgg"
+countstr9 = str9.count("g")
+print(countstr9)
+
+str10 = "naruto is the fifth hokage"
+print(str10.endswith("hokage"))
+
+str11 = "sahsuke uchiha great, but not better than naruto uzhumaki"
+print(str11.find("naruto uzhumaki"))
+
+str12 = "never underestimate naruto, he kill you"
+print(str12.index("naruto"))
+
+str13 = "TheCopyCatNinjaIsKakashiHatake"
+print(str13.isalnum())
+
+str14 = "TheGhostOfUchihaIsMadaraUchiha"
+print(str14.isalpha())
+
+str15 = "wake up to reality"
+print(str15.islower())
+
+str16 = "WAKE UP TO REALITY"
+print(str16.isupper())
+
+str17 = "thats my nindo my ninja way"
+print(str17.isprintable())
+
+str18 = "           "
+print(str18.isspace())
+
+str19 = "Sahske Uchiha The Brother Of Itachi Uchiha"
+print(str19.istitle())
+
+str20 = "the taijutsu king rock lee"
+print(str20.startswith("the"))
+
+str21 = "naruto and nine tell fox are dragons"
+print(str21.swapcase())
+
+str22 = "naruto is not a anime it is emotion"
+print(str22.title())
+
+
