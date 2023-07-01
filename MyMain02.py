4fdf93082accc9f8bd8281f4dc8fb0c273310aa2
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-01 17:07:24 +0530

    Add files via upload

---
+++ MyMain02.py
@@ -0,0 +1,20 @@
+a = 1 
+b = True
+c = "akash"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))
+print("the type of c is ", type(c))
+
+list1 = [8,2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple1)
+
+dict1 = {"name":"sakshi", "age":20, "canVote":True}
+print(dict1)
