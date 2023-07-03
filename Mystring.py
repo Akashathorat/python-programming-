0fbb753a8e3adc6a05eb4f6c04ceead90dc469d4
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-03 11:18:44 +0530

    Add files via upload

---
+++ Mystring.py
@@ -0,0 +1,19 @@
+name = "akash"
+friend = "kunal"
+anotherFriend = 'aniket'
+apple = '''he said,
+hi akash 
+hey i am good 
+"i want to eat an apple'''
+
+print("hello, " + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("lets use a for loop\n")
+for character in apple:
+        print(character)
