52b0a13f95e200082bee780b75eddee1b0ba2b38
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-03 11:30:57 +0530

    Add files via upload

---
+++ MyFunction.py
@@ -0,0 +1,25 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b) 
+    print(mean)
+
+def ifGreater(a,b):
+ if(a>b):
+    print("first number is greater")
+ else:
+    print("second number is greater or equal")
+    
+def iflesser(a, b):
+    pass
+    
+a = 9
+b = 8
+ifGreater(a, b)
+calculateGmean(a, b)
+# gmean1 =(a*b)/(a+b) 
+# print(gmean1)
+c = 8
+d = 74
+ifGreater(c, d)
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+b) 
+# print(gmean2)
