1f740d2a644f25b16b46bba4dbb79f6bb42ea83c
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-03 11:21:19 +0530

    Add files via upload

---
+++ MyIf-Else.py
@@ -0,0 +1,15 @@
+a = int(input("enter your age: "))
+print("your age is:", a)
+# conditional operators
+# >, <, >=, <=, ==,!=
+# print(a>18)
+# print(a<=18)
+# prnt(a==18)
+# print(a!=18)
+if(a>18):
+    print("you can drive")
+    print("yes")
+else:
+    print("you cannot drive")    
+    print("no")
+    print("yay!")
