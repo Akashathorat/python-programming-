9b02a5e9b915cdf7d001840953b2dfd11655728e
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-03 11:22:46 +0530

    Add files via upload

---
+++ MyForLoop.py
@@ -0,0 +1,12 @@
+ # name = "akash"
+ # for i in  name:
+ #     print(i, end=",")
+
+color = ["red", "blue", "green", "yellow"]
+for color in color:
+ print(color)
+ for i in color:
+     print(i)
+
+ # for k in range(1, 50, 5):
+    # print(k)
