14eb7aa50ce8435b23d20e4165858813de2cd295
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-03 11:23:08 +0530

    Add files via upload

---
+++ MyWhileLoop.py
@@ -0,0 +1,15 @@
+i = int(input("enter the number: "))
+while(i<=30):
+ i = int(input("emter the number: "))
+ print(i)
+
+print("done with the loop")
+
+# count = 5
+# while (count > 0):
+#     print(count)
+#     count = count - 1
+
+# do {
+    # loop body;
+# }while(condition);
