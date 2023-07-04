3aff64fe8326ae99f986acad3ebca7a168b35244
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-04 15:46:44 +0530

    Add files via upload

---
+++ MyFunction04(Variable-LenArgument).py
@@ -0,0 +1,21 @@
+def average(*numbers):
+  # print(type(numbers))
+
+   sum = 0
+   for i in numbers:
+    sum = sum + i
+  #  print("average is:", sum / len(numbers))
+  # return 7
+   print("average is:", sum / len(numbers))
+
+# average(4, 6)
+# average(b=9)
+
+
+c = average(5, 6, 7, 1)
+print(c)
+# def name(**name)
+# #  print(type(name))
+# print("hello,", name["fname"],  name["mname"],  name["lname"])
+
+# name(mname="akash", lname="aniket", fname="kunal")
