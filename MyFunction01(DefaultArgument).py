c54eec9b405faa37975a441e84923c070a45c65d
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-04 15:45:05 +0530

    Add files via upload

---
+++ MyFunction01(DefaultArgument).py
@@ -0,0 +1,9 @@
+def average(a=9, b=1):
+    print("the average is", (a+b)/2)
+# average(4, 6)
+average(b=9)
+
+def name(fname, mname = "jhon", lname = "whatson"):
+    print("hello", fname, mname, lname)
+
+name("amy", "agrawal")
