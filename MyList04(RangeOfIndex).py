7f0eab7e8a062c940a775bd33a5fcc8e788c5dab
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-13 11:18:49 +0530

    Add files via upload

---
+++ MyList04(RangeOfIndex).py
@@ -0,0 +1,6 @@
+animals = ["dog", "cat", "cow", "lion", "tiger", "goat", "snake", "zebra", "horse", "bat", "giraf"]
+          # [0]     [1]   [2]    [3]      [4]     [5]      [6]      [7]      [8]
+
+print(animals)
+print(animals[1 : 8])
+print(animals[1 : 8 : 2])   # 2 is the jump index
