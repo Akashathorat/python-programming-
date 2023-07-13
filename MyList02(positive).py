8c2a1d589d1c426ad1e1e95b4658e8021e1e34dd
Akash Thorat <116056217+Akashathorat@users.noreply.github.com>
2023-07-13 11:18:01 +0530

    Add files via upload

---
+++ MyList02(positive).py
@@ -0,0 +1,9 @@
+colors = ["red","green","yellow","blue","pink"]
+            # [0]   [1]     [2]     [3]    [4]
+print(colors[3])
+print(colors[1])
+print(colors[2])
+print(colors[4])
+print(colors[0])
+    
+    
